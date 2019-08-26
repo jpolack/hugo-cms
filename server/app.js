0d0e03951b019a99d1b1f0f3c9b27b5be80235d3
Jonah <mail@jonahpolack.de>
2019-08-26 12:53:07 +0200

    fixed webpack require

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
-const webpack = require('webpack');
-const middleware = require('webpack-dev-middleware');
-const webpackConfig = require('../app/webpack.config');
+module.exports = () => {
+  const webpack = require('webpack'); // eslint-disable-line global-require, import/no-extraneous-dependencies
+  const middleware = require('webpack-dev-middleware'); // eslint-disable-line global-require, import/no-extraneous-dependencies
+  const webpackConfig = require('../app/webpack.config'); // eslint-disable-line global-require, import/no-extraneous-dependencies
 
-const compiler = webpack(webpackConfig);
-
-module.exports = middleware(compiler);
+  const compiler = webpack(webpackConfig);
+  return middleware(compiler);
+};
